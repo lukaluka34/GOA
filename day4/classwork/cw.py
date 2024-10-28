da6cf9a13beaa035bac21b5092e697230a9d333c
lukaluka34 <lukachaganav@gmail.com>
2024-10-28 23:32:32 +0400

    commit

---
+++ day4/classwork/cw.py
@@ -0,0 +1,12 @@
+num1="luka"
+num2="chaganava"
+
+print=(num1+num2)
+ 
+print(1+1)
+print(2*2)
+print(10-5)
+print(20/2)
+
+int1=40
+int2=10
---
+++ day4/homework/hw4.py
@@ -0,0 +1,4 @@
+mother_age =35
+father_age =45
+brother_age =10
+print(father_age,father_age,brother_age)
