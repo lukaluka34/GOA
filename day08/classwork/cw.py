e47298f6878a3903bf291563d46516c83dfef24a
lukaluka34 <lukachaganav@gmail.com>
2024-11-26 15:23:53 +0400

    commit

---
+++ day08/classwork/cw.py
@@ -0,0 +1,4 @@
+print(input("enter your name"))
+name1=input("please enter your surnamme")
+name2=input("please enter your surnamme")
+print(name1+name2)
---
+++ day08/homework/hw2.py
@@ -0,0 +1,5 @@
+print(input("please enter your name:"))
+
+name1 = input("enter your surname:")
+name2 = input("enter uour age:")
+print (name1+name2)
