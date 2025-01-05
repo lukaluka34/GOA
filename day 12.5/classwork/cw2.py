5e543fe45547175665fa26508492c8c67888fca6
lukaluka34 <lukachaganav@gmail.com>
2025-01-05 18:23:04 +0400

    commit

---
+++ day 12.5/classwork/cw2.py
@@ -0,0 +1,3 @@
+date = int(input("enter your date:"))
+birthday = int(input("enter your birthday:20"))
+print(date-birthday)
---
+++ day 12.5/classwork/cw3.py
@@ -0,0 +1,3 @@
+num1 = int(input("enter your number:"))
+num2 = int(input("enter your number:"))
+print(num1*num2)
