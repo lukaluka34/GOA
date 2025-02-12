0252d4aea9dcd3c9a76083f326baadb9d7c77f67
lukaluka34 <lukachaganav@gmail.com>
2025-02-12 14:18:16 +0400

    commit

---
+++ day 19/classwork/cw.py
@@ -0,0 +1,5 @@
+data = [42, 3.14, "Hello", True, None, ["apple", "banana"], {"name": "Alice"}, (1, 2, 3), set([1, 2, 3]), bytearray(b"World")]
+
+last_five = data[-5:]
+
+print("ბოლო 5 მნიშვნელობა:", last_five)
--- day 19/homework/hw5.py
+++ day 19/homework/hw5.py
@@ -1,23 +1,8 @@
-numbers = []  
+numbers = []
 
-
-for _ in range(10):  
-    num = int(input("შეიყვანეთ რიცხვი: "))
+for i in range(10):
+    
+    num = int(input("Please enter a number: ")) 
     numbers.append(num)
 
-
-to_find = int(input("რომელ რიცხვს ეძებთ? "))  
-
-
-found = False 
-for num in numbers:
-    if num == to_find:
-        found = True
-        break  
-
-
-if found:
-    print(f"{to_find} არის სიაში.")
-else:
-    print(f"{to_find} არ არის სიაში.")
-
+print("The collected numbers are:", numbers)
