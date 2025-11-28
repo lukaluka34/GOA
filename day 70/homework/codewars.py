7f02a770acf83fc0042e742ce352f2f1dd76bf18
lukaluka34 <lukachaganav@gmail.com>
2025-11-28 22:43:19 +0400

    commit

---
+++ day 70/homework/codewars.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/5208f99aee097e6552000148/train/python
+
+# def solution(s):
+#     result = ""
+#     for i in s:
+#         if i.isupper():
+#             result += " " + i
+#         else:
+#             result += i
+#     return result
