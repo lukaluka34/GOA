fd1856ca3c1e0a329fa63cb8de7878e778ea644e
lukaluka34 <lukachaganav@gmail.com>
2025-12-01 20:16:32 +0400

    commit

---
+++ day 70/classwork/codewars.py
@@ -0,0 +1,8 @@
+
+# https://www.codewars.com/kata/557a2c136b19113912000010
+
+#  https://www.codewars.com/kata/55b42574ff091733d900002f 
+#  https://www.codewars.com/kata/59a8570b570190d313000037 
+# https://www.codewars.com/kata/55955a48a4e9c1a77500005a 
+  
+#  https://www.codewars.com/kata/54da5a58ea159efa38000836 
